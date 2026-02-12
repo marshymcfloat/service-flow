387de09d7cf73efecd4e76475efa84c394b39d56
Daniel Canoy <canoydaniel06@gmail.com>
2026-02-12 09:48:18 +0800

    for github workflow ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./test/setup.ts"],
-    exclude: [...configDefaults.exclude, "test/e2e/**"],
+    exclude: [...configDefaults.exclude, "test/e2e/**", "tests/**"],
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov"],
